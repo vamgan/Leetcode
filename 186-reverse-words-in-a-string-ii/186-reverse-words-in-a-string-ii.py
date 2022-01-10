bd9b5d4e251822d451357248b36d9c3095e9f165
Vamil Gandhi <hello@vamilgandhi.com>
2022-01-10 15:48:48 +0530

    Time: 253 ms (27.99%), Space: 18.4 MB (59.25%) - LeetHub

---
+++ 186-reverse-words-in-a-string-ii/186-reverse-words-in-a-string-ii.py
@@ -0,0 +1,28 @@
+class Solution:
+    def reverseWords(self, s: List[str]) -> None:
+        """
+        Do not return anything, modify s in-place instead.
+        """
+        for i in range(len(s)//2):
+            s[i], s[len(s) - 1 - i] =  s[len(s) - 1 - i], s[i]
+        start = 0
+        end = 0
+        i = 0
+        while i < len(s):
+            if s[i] == " ":
+                print(i, start, end)
+                end -= 1
+                while start <= end:
+                    s[end], s[start] = s[start], s[end]
+                    end -= 1
+                    start += 1
+                start = i + 1
+                end = i
+            i += 1
+            end += 1
+        end -= 1
+        while start <= end:
+            s[end], s[start] = s[start], s[end]
+            end -= 1
+            start += 1
+        
