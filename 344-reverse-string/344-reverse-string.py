879e40a9f224609fecfed10fc9e2e27da7c6b6df
Vamil Gandhi <hello@vamilgandhi.com>
2022-01-10 14:55:34 +0530

    Time: 313 ms (14.89%), Space: 18.6 MB (63.19%) - LeetHub

--- 344-reverse-string/344-reverse-string.py
+++ 344-reverse-string/344-reverse-string.py
@@ -3,6 +3,7 @@ class Solution:
         """
         Do not return anything, modify s in-place instead.
         """
-        return s.reverse()
+        for i in range(len(s)//2):
+            s[i],s[len(s) - 1 - i] = s[len(s) - 1 - i], s[i]
             
         
