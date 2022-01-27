86586a4c27ee776d35a89ae8b4d375031530fbf9
Vamil Gandhi <hello@vamilgandhi.com>
2022-01-27 14:05:03 -0500

    Time: 28 ms, Memory: 13.9 MB - LeetHub

--- add-binary/add-binary.py
+++ add-binary/add-binary.py
@@ -1,14 +1,5 @@
 class Solution:
     def addBinary(self, a: str, b: str) -> str:
-        # if len(a) < 4:
-        #     a = Solution.formatBinary(a)
-        # if len(b) == 1:
-        #     b = Solution.formatBinary(b)
-        # print(a,b)
         num1 = int(a,2)
         num2 = int(b,2)
-        print(num1,num2)
         return bin(num1 + num2).replace("0b","")
-    def formatBinary(num):
-        diff = 4 - len(num)
-        return "0" * diff + num
