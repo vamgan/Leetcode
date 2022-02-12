399c27706bd11bfdbf7a58f411878e374f02e9f8
Vamil Gandhi <hello@vamilgandhi.com>
2022-02-12 17:29:29 -0500

    Time: 72 ms (85.36%), Space: 26.4 MB (42.23%) - LeetHub

--- 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
+++ 236-lowest-common-ancestor-of-a-binary-tree/236-lowest-common-ancestor-of-a-binary-tree.py
@@ -1,28 +1,23 @@
+# Definition for a binary tree node.
+# class TreeNode:
+#     def __init__(self, x):
+#         self.val = x
+#         self.left = None
+#         self.right = None
+
 class Solution:
-
-    def __init__(self):
+    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
         self.ans = None
-
-    def lowestCommonAncestor(self, root, p, q):
-        """
-        :type root: TreeNode
-        :type p: TreeNode
-        :type q: TreeNode
-        :rtype: TreeNode
-        """
-        def recurse_tree(current_node):
-            if not current_node:
+        
+        def dfs(node):
+            if not node:
                 return False
-
-            left = recurse_tree(current_node.left)
-
-            right = recurse_tree(current_node.right)
-
-            mid = current_node == p or current_node == q
-
-            if mid + left + right >= 2:
-                self.ans = current_node
+            left = dfs(node.left)
+            right = dfs(node.right)
+            
+            mid = node == p or node == q
+            if mid + left + right >=2:
+                self.ans = node
             return mid or left or right
-
-        recurse_tree(root)
-        return self.ans
+        dfs(root)
+        return self.ans or p or q
