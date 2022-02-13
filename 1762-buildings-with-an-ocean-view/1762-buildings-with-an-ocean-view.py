565e0cdbaff63a85bc5ea3698b1a5c696b5cbb48
Vamil Gandhi <hello@vamilgandhi.com>
2022-02-13 02:34:35 -0500

    Time: 1482 ms (9.63%), Space: 31.6 MB (48.82%) - LeetHub

--- 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
+++ 1762-buildings-with-an-ocean-view/1762-buildings-with-an-ocean-view.py
@@ -4,8 +4,6 @@ class Solution:
         maxHeight = heights[-1]
         for i in reversed(range(len(heights) - 1)):
             if heights[i] > heights[i+1] and heights[i] > maxHeight:
-                print(heights[i])
-                print(i)
                 res.append(i)
                 maxHeight = heights[i]
         res.reverse()
