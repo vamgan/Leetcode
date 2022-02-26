15f2f5467461db7dfc4e0175bbfd39aea3aa8a4c
Vamil Gandhi <hello@vamilgandhi.com>
2022-02-26 19:46:48 +0530

    Time: 1618 ms (85.89%), Space: 28.5 MB (45.96%) - LeetHub

---
+++ 1891-cutting-ribbons/1891-cutting-ribbons.py
@@ -0,0 +1,22 @@
+class Solution:
+    def maxLength(self, ribbons: List[int], k: int) -> int:
+        def possible(rlen):
+            total = 0
+            for i in ribbons:
+                total += i // rlen
+            print(total)
+            return total < k
+        low, high = 1, max(ribbons)
+        if k > sum(ribbons):
+            return 0
+
+        while low <= high:
+            mid = (low + high) // 2
+            print(mid)
+            if possible(mid):
+                high = mid - 1
+            else:
+                low = mid + 1
+        return high
+        
+                
