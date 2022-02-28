14304fbff97f59dbf31822f0b2e381a98a605bce
Vamil Gandhi <hello@vamilgandhi.com>
2022-02-28 14:22:44 +0530

    Time: 208 ms (42.74%), Space: 14.5 MB (62.49%) - LeetHub

--- 680-valid-palindrome-ii/680-valid-palindrome-ii.py
+++ 680-valid-palindrome-ii/680-valid-palindrome-ii.py
@@ -1,29 +1,24 @@
 class Solution:
     def validPalindrome(self, s: str) -> bool:
-        left = 0
-        right = len(s) - 1
-        invalid = 0
-        def isValid(s):
+        def check(st):
             i = 0
-            j = len(s) - 1
-            while i < j:
-                if s[i] != s[j]:
+            j = len(st) - 1
+            while i <= j:
+                if st[i] != st[j]:
                     return False
                 i += 1
                 j -= 1
             return True
-        while left < right:
-            if s[left] != s[right]:
-                left = isValid(s[:left] + s[left + 1:])
+        i = 0
+        j = len(s) - 1
+        while i <= j:
+            if s[i] != s[j]:
+                left = check(s[:i] + s[i+1:])
                 if left:
                     return True
-                right = isValid(s[:right] + s[right + 1:])
-                if right:
+                if check(s[:j] + s[j+1:]):
                     return True
                 return False
-            left += 1
-            right -= 1
+            i += 1
+            j -= 1
         return True
-        
-        
-        
