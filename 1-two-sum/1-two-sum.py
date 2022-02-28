466d4e72148b7578d6fd6d8aa4f8da6a27e16f70
Vamil Gandhi <hello@vamilgandhi.com>
2022-02-28 12:49:11 +0530

    Time: 97 ms (59.18%), Space: 15.1 MB (64.38%) - LeetHub

--- 1-two-sum/1-two-sum.py
+++ 1-two-sum/1-two-sum.py
@@ -1,10 +1,9 @@
 class Solution:
     def twoSum(self, nums: List[int], target: int) -> List[int]:
         mapper = {}
-        for idx, num in enumerate(nums):
-            diff = target - num
-            if diff in mapper:
-                return [mapper[diff], idx]
-            else:
-                mapper[num] = idx
-                
+        s = 0
+        for i, n in enumerate(nums):
+            complement = target - n
+            if complement in mapper:
+                return [mapper[complement], i]
+            mapper[n] = i
