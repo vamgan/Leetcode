3b97369d179fabb695b7f7a94ec16d29cc889b6c
Vamil Gandhi <hello@vamilgandhi.com>
2022-03-01 16:46:59 +0530

    Time: 96 ms (51.93%), Space: 18.3 MB (83.94%) - LeetHub

--- 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
+++ 1650-lowest-common-ancestor-of-a-binary-tree-iii/1650-lowest-common-ancestor-of-a-binary-tree-iii.py
@@ -1,22 +1,8 @@
-"""
-# Definition for a Node.
-class Node:
-    def __init__(self, val):
-        self.val = val
-        self.left = None
-        self.right = None
-        self.parent = None
-"""
-
 class Solution:
     def lowestCommonAncestor(self, p: 'Node', q: 'Node') -> 'Node':
-        a_node = p
-        b_node = q
-        a_node_set = set()
-        while a_node:
-            a_node_set.add(a_node.val)
-            a_node = a_node.parent
-        while b_node:
-            if b_node.val in a_node_set:
-                return b_node
-            b_node = b_node.parent
+        ptr_p, ptr_q = p, q
+        while ptr_p is not ptr_q:
+            ptr_p = ptr_p.parent if ptr_p.parent else q
+            ptr_q = ptr_q.parent if ptr_q.parent else p
+            
+        return ptr_q
