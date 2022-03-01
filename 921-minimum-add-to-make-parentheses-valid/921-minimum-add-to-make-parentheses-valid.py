017c769902457c4ba34e5f912af2ca19af507328
Vamil Gandhi <hello@vamilgandhi.com>
2022-03-01 17:49:38 +0530

    Time: 43 ms (55.60%), Space: 13.8 MB (98.28%) - LeetHub

--- 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
+++ 921-minimum-add-to-make-parentheses-valid/921-minimum-add-to-make-parentheses-valid.py
@@ -3,11 +3,11 @@ class Solution:
         opening = 0
         count = 0
         for i in s:
-            if i == '(':
+            if i == "(":
                 opening += 1
-            elif i == ')':
-                if opening <= 0:
-                    count += 1
+            elif i == ")":
+                if opening:
+                    opening -=1
                 else:
-                    opening -= 1
+                    count += 1
         return opening + count
