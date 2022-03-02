4bc06ea19a365c16a978df5be3f8d08b7071c57a
Vamil Gandhi <hello@vamilgandhi.com>
2022-03-02 18:02:26 +0530

    Time: 454 ms (46.12%), Space: 16.5 MB (66.41%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,23 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
+        row = len(grid) - 1
+        column = len(grid[0]) - 1
+        neigh = [(1,0), (-1,0), (0,1), (0,-1)]
         count = 0
-        for i in range(len(grid)):
-            for j in range(len(grid[0])):
-                if grid[i][j] == "1":
+        def dfs(i,j):
+            nonlocal row, column, neigh
+            if i < 0 or i > row or j < 0 or j > column or grid[i][j] == "0":
+                return
+            grid[i][j] = "0"
+            for r, c in neigh:
+                dfs(i+r,j+c)
+        for r in range(row + 1):
+            for c in range(column + 1):
+                if grid[r][c] == "1":
                     count += 1
-                    self.dfs(grid,i,j)
+                    dfs(r,c)
         return count
-    
-    def dfs(self,grid,rows, columns):
-        if rows < 0 or columns < 0 or rows >= len(grid) or columns >= len(grid[0]) or grid[rows][columns] == "0":
-            return
-        else:
-            grid[rows][columns] = "0"
-            self.dfs(grid, rows - 1, columns)
-            self.dfs(grid, rows + 1, columns)
-            self.dfs(grid, rows, columns -1)
-            self.dfs(grid, rows, columns + 1)
-            
-            
-
-    
