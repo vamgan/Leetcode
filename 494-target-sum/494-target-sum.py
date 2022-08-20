a84f284b32fb699faaece362f6c0d9e3e875001a
Vamil Gandhi <hello@vamilgandhi.com>
2022-08-20 18:06:43 -0400

    Time: 388 ms (68.54%), Space: 35.9 MB (41.92%) - LeetHub

--- 494-target-sum/494-target-sum.py
+++ 494-target-sum/494-target-sum.py
@@ -2,11 +2,11 @@ class Solution:
     def findTargetSumWays(self, nums: List[int], target: int) -> int:
         dp = {}
         def dfs(idx, running_sum):
-            if idx == len(nums):
+            if idx + 1 == len(nums):
                 if running_sum == target:
                     return 1
                 return 0
             if (idx, running_sum) not in dp:
-                dp[(idx, running_sum)] = dfs(idx + 1, running_sum + nums[idx]) + dfs(idx + 1, running_sum - nums[idx])
+                dp[(idx, running_sum)] = dfs(idx + 1, running_sum + nums[idx +1]) + dfs(idx + 1, running_sum - nums[idx + 1])
             return dp[(idx, running_sum)]
-        return dfs(0, 0)
+        return dfs(-1, 0)
