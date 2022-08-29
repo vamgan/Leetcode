04382ecc4fee2a68cf814de2a18dc0db79e0577b
Vamil Gandhi <hello@vamilgandhi.com>
2022-08-29 13:54:49 -0400

    Time: 111 ms (14.39%), Space: 13.9 MB (66.59%) - LeetHub

--- 1002-find-common-characters/1002-find-common-characters.py
+++ 1002-find-common-characters/1002-find-common-characters.py
@@ -1,26 +1,26 @@
 class Solution:
     def commonChars(self, words: List[str]) -> List[str]:
-        final = {}
+        first_map = {}
         for letter in words[0]:
-            if letter in final:
-                final[letter] += 1
+            if letter in first_map:
+                first_map[letter] += 1
             else:
-                final[letter] = 1
+                first_map[letter] = 1
         for word in words[1:]:
             temp = {}
             for letter in word:
-                if letter in temp:
-                    temp[letter] += 1
-                else:
-                    temp[letter] = 1
-            for key in final:
+                if letter in first_map:
+                    if letter in temp:
+                        temp[letter] += 1
+                    else:
+                        temp[letter] = 1
+            for key in first_map:
                 if key in temp:
-                    final[key] = min(final[key], temp[key])
+                    first_map[key] = min(first_map[key], temp[key])
                 else:
-                    final[key] = 0
-        print(final)
+                    first_map[key] = 0
         res = []
-        for key, val in final.items():
+        for key, val in first_map.items():
             if val > 0:
                 temp = [key] * val
                 res.extend(temp)
