131df079dbe20ffaadee35a960d5eb0a5c8ae797
Vamil Gandhi <hello@vamilgandhi.com>
2022-08-29 00:33:55 -0400

    Time: 296 ms (96.57%), Space: 16.5 MB (65.84%) - LeetHub

--- 200-number-of-islands/200-number-of-islands.py
+++ 200-number-of-islands/200-number-of-islands.py
@@ -1,19 +1,19 @@
 class Solution:
     def numIslands(self, grid: List[List[str]]) -> int:
-        row = len(grid) - 1
-        column = len(grid[0]) - 1
-        neigh = [(1,0), (-1,0), (0,1), (0,-1)]
-        count = 0
-        def dfs(i,j):
-            nonlocal row, column, neigh
-            if i < 0 or i > row or j < 0 or j > column or grid[i][j] == "0":
+        rows = len(grid)
+        cols = len(grid[0])
+        res = 0
+        def dfs(i, j):
+            if i < 0 or j < 0 or i >= rows or j >= cols or grid[i][j] == '0':
                 return
-            grid[i][j] = "0"
-            for r, c in neigh:
-                dfs(i+r,j+c)
-        for r in range(row + 1):
-            for c in range(column + 1):
-                if grid[r][c] == "1":
-                    count += 1
-                    dfs(r,c)
-        return count
+            grid[i][j] = '0'
+            dfs(i + 1, j)
+            dfs(i - 1, j)
+            dfs(i, j + 1)
+            dfs(i, j - 1)
+        for i in range(rows):
+            for j in range(cols):
+                if grid[i][j] == '1':
+                    res += 1
+                    dfs(i,j)
+        return res
