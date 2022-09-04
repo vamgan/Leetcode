6a95d559f7c4dd3470b30765935346a0bcf60535
Vamil Gandhi <hello@vamilgandhi.com>
2022-09-04 17:48:20 -0400

    Time: 394 ms (87.97%), Space: 14.7 MB (86.25%) - LeetHub

--- 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
+++ 159-longest-substring-with-at-most-two-distinct-characters/159-longest-substring-with-at-most-two-distinct-characters.py
@@ -7,11 +7,9 @@ class Solution:
             dic[s[right]] = right
             if len(dic) == 3:
                 max_len = max(max_len, right - left)
-                temp_left = right
-                for key in dic:
-                    temp_left = min(dic[key], temp_left)
-                del dic[s[temp_left]]
-                left = temp_left + 1
+                left = min(dic.values())
+                del dic[s[left]]
+                left += 1
             right += 1
         max_len = max(max_len, right - left)
         return max_len
